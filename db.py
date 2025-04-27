703f51db6e9a4a3425091fdcf8591b065c647fe1
Willboydie <williamteboyd@gmail.com>
2025-04-27 23:12:24 +0100

    added version control tools

--- db.py
+++ db.py
@@ -1,6 +1,7 @@
 import pymysql
 
 # Central DB config
+# We can also place sensitive db credentials in a .env file and load them using dotenv
 db_config = {
     'host': 'eubcdb-2.cp6ymm2sk6ub.eu-west-2.rds.amazonaws.com',
     'user': 'admin',
